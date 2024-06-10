906cdc4248281f0daf9e7a2dd9836ce270894057
laliteshx6 <2200031554@kluniversity.in>
2024-06-10 18:30:43 +0530

    deploy

--- LandingPage/urls.py
+++ LandingPage/urls.py
@@ -20,9 +20,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path("__reload__/", include("django_browser_reload.urls")),
-                  path('', include("App.urls")),
-              ] 
-urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('admin/', admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
+    path('', include("App.urls")),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
